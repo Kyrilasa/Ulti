75c1cd704aa345456a83ab8e79b686e2770e081b
Kyrilasa <szucsbarnabas98@gmail.com>
2022-12-12 14:23:35 +0100

    Built web
    Branch: master
    Commit: 60db9a05336a6d124a06b770899bec315522da6d (dirty)
    
    package:peanut 4.3.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4af817f8de4133edacca0908f16858d5",
-"index.html": "f803752c8889d1764cf29136085dea8c",
-"/": "f803752c8889d1764cf29136085dea8c",
+"index.html": "6c6f1ffa4f94e3baee5b069420475bbe",
+"/": "6c6f1ffa4f94e3baee5b069420475bbe",
 "main.dart.js": "6efad515e02c5d859bc3109fbc14d86b",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
